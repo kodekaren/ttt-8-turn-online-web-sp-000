6ac795384ed670681ec7e20312222d69098056ae
karen ercoli <karenercoli@gmail.com>
2018-11-10 14:47:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,17 +29,3 @@ end
 def move(board, index, token = "X")
   board[index] = token
 end
-
-def turn(board)
-  puts "Please enter 1-9:"
- input =gets.strip
- index = input_to_index(input)
-  if
-valid_move? == true
-move(board, index,token)
-  else
-    turn(board)
-  end
-end
-  display_board(board)
-end
